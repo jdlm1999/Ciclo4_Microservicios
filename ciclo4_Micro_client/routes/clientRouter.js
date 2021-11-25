b518981d45a45e2bec84cc015615cb543663e6ce
Camilo <el.camilo2001@hotmail.com>
2021-11-24 19:12:00 -0500

    Prueba

--- ciclo4_Micro_client/routes/clientRouter.js
+++ ciclo4_Micro_client/routes/clientRouter.js
@@ -2,5 +2,9 @@ const express = require('express');
 const router = express.Router();
 const Client = require('../controllers/clientController');
 
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+    res.send('respond with a resource');
+  });
 
 module.exports = router;
